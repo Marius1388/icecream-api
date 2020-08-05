cb31fc5a134ca1662cc0280a86fd06c39e42f975
Marius1388 <mariuscristian.bancila@gmail.com>
2020-08-05 13:50:46 +0300

    cors domain update

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ app.use(function (req, res, next) {
 	);
 
 	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With,Content-Type,Authorization'
+	);
 
 	// Set to true if you need the website to include cookies in the requests sent
 	// to the API (e.g. in case you use sessions)
